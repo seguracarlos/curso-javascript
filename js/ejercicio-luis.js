b16f5f4e051e70be2d8bc6716e1d4413a35394f7
seguracarlos <segurac.carlos@gmail.com>
2019-11-08 16:37:20 -0600

    cambio

---
+++ js/ejercicio-luis.js
@@ -0,0 +1,25 @@
+'use strict'
+
+var numero = parseInt(prompt('ingresa un numero del 2 al 9',0));
+
+var contador = 0;
+
+
+if(numero >=2 && numero <=9){
+
+	for(var i=2; i<=9; i++){
+
+		var res_usuario = parseInt(prompt(numero+' x '+i));
+
+		var resultado =numero*i;
+		if(resultado == res_usuario){
+
+			contador++;
+		}
+
+	}
+
+	alert("obtuviste un total de: "+contador+" aciertos");
+}else{
+
+alert("el numero que ingresaste no corresponde al rango seleccionado");}

ca1a717682874fda08f9a1edb356825a414adff4
seguracarlos <segurac.carlos@gmail.com>
2019-12-17 18:19:52 -0600

    cometario

--- aprendiendo-jquery/js/05-ajax.js
+++ aprendiendo-jquery/js/05-ajax.js
@@ -1,4 +1,28 @@
 $(document).ready(function(){
 	// metodo load, nos permite hacer peticion ajax por get
-	$('#datos').load("https://reqres.in/");
+	//$('#datos').load("https://reqres.in/");
+
+	$.get("https://reqres.in/api/users",{page: 2}, function(response){
+		response.data.forEach((element, index)=>{
+			$('#datos').append("<p>"+element.first_name+" "+element.last_name+"</P>");
+		});
+	});
+
+	
+
+	$.("#formulario").submit(function(){
+		
+
+		var usuario ={
+		name: $('input[name="name"]').val();
+		apellido:$('input[name="apellido"]').val();
+		web:$('input[name="web"]').val();
+		};
+
+		$.post($(this).attr("action"), usuario, function(response){
+		console.log(response);
+		});
+
+		return false;
+	});
 });
